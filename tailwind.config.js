705f3080a0fbade2f07dc6ce70e4b192c116f44d
ice-bear98 <122257455+ice-bear98@users.noreply.github.com>
2024-04-04 21:56:13 +0900

    Feat: tailwind config 파일에 반응형 extand 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "640px",
+            md: "800px",
+            lg: "1100px",
+            xl: "1280px",
+        },
+        extend: {
+            backgroundImage: {
+                "home-image": "url(./src/assets/img/Home.jpg)",
+            },
+        },
     },
     plugins: [],
 };
